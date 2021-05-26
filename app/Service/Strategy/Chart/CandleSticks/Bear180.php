b4cef401e063e2cead2f4a9922c66de6f4f4c77a
MayconVM <mayconvm@gmail.com>
2021-05-26 07:54:29 -0300

    Add strategy to Bear180.

--- app/Service/Strategy/Chart/CandleSticks/Bear180.php
+++ app/Service/Strategy/Chart/CandleSticks/Bear180.php
@@ -4,17 +4,44 @@ namespace App\Service\Strategy\Chart\CandleSticks;
 
 class Bear180 extends CandleSticksAbstractInterface
 {
-    public function watch() : bool
+    public function watch(int $timeSecond = 1, int $quantatySticks = 3, float $percLengthDiff = 2)
     {
+        while (true) {
+            yield $this->exist($quantatySticks, $percLengthDiff);
+
+            sleep($timeSecond);
+        }
     }
 
-    public function exist() : bool
+    public function exist(int $quantatySticks = 3, float $percLengthDiff = 2) : bool
     {
-        dd(
-            $this->ma(20),
-            $this->ema(20),
-            $this->wma(20),
-            $this->avl(20),
-        );
+        $data = array_values($this->getData($quantatySticks));
+        $percLengthDiff = $percLengthDiff/100;
+
+        // TODO: essa classe não deve conhecer a estrutura dos dados que veio da binance
+        $diff = [];
+        foreach ($data as $key => $value) {
+            if (($key - 1) < 0) {
+                continue;
+            }
+
+            if ($value['close'] < $value['open']
+                && $value['close'] < ($data[$key - 1]['close'] ?? 0)
+                && (1 - ($value['close'] / $value['open'])) > $percLengthDiff
+                && (1 - ($data[$key - 1]['open'] / $data[$key - 1]['close'])) > $percLengthDiff
+            ) {
+                // echo $key . ' - ' .
+                //     $value['close'] . ' - ' .
+                //     $value['open'] . ' - ' .
+                //     $percLengthDiff . ' - ' .
+                //     (1 - ($value['close'] / $value['open'])) . ' - ' .
+                //     (1 - ($data[$key - 1]['open'] / $data[$key - 1]['close'])) . ' - ' .
+                // "\n<br>";
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
